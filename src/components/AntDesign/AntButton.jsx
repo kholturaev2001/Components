679bd9517d58786bc09ef9eb35930c6f44ee009b
mirakov <holturaevm@gmail.com>
2022-12-26 20:20:26 +0500

    working on AntButton

--- src/components/AntDesign/AntButton.jsx
+++ src/components/AntDesign/AntButton.jsx
@@ -1,9 +1,18 @@
+import { UploadOutlined } from "@mui/icons-material";
 import { Button } from "antd";
 import React from "react";
+import { useState } from "react";
+import './AntButton.css'
 
 const AntButton = () => {
+  const [loading, setLoading] = useState(false);
+
   const onButtonClick = (e) => {
     console.log("Button Clicked!");
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
   };
   return (
     <div>
@@ -11,7 +20,15 @@ const AntButton = () => {
         <Button>Default!</Button>
       </div>
       <div>
-        <Button onClick={onButtonClick} block type="primary">
+        <Button
+          loading={loading}
+          icon={<UploadOutlined />}
+          onClick={onButtonClick}
+          block
+          type="primary"
+        //   style={{ display: "flex", alignItems: "center", color: "red" }}
+          className='primaryBtn'
+        >
           Primary!
         </Button>
       </div>
